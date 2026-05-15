aa27ed02ee5b29ad11206ed98871c1154431b936
G.Gimsara <lenmicr250@gmail.com>
2026-05-15 15:16:37 +0530

    add backend exception handling

---
+++ backend/src/main/java/com/meditrack/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.meditrack.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
